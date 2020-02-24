1bd0d4822d33a7435aa117e4aea253b4d9900941
Alex Evans <alex.evans1230@gmail.com>
2020-02-24 19:01:50 +0500

    Added callbacks for android

--- index.js
+++ index.js
@@ -1,6 +1,78 @@
+import {
+    NativeModules,
+    NativeEventEmitter,
+    Platform
+} from "react-native"
+const notificationModule = NativeModules.BlitzNotifications
+const eventEmitter = new NativeEventEmitter(notificationModule);
 
-import { NativeModules } from 'react-native';
 
-const { BlitzNotifications } = NativeModules;
+export default {
+    getDeviceId: (callback) => {
+        if (Platform.OS === 'android') {
+            if (notificationModule) {
+                if (notificationModule.registerForToken) {
+                    notificationModule.registerForToken(deviceId => {
+                        callback(deviceId)
+                    })
+                } 
+            }
+        } else {
+            let event = eventEmitter.addListener("deviceRegisteredIOS", deviceId => {
+                callback(deviceId)
+                eventEmitter.removeSubscription(event)
+            })
+            notificationModule.registerForToken()
+        }
+    },
+    getLastNotificationData: (callback, errorCallback) => {
+        if (Platform.OS === 'android') {
+            notificationModule.getIntent(notification => {
+                try {
+                    if (typeof notification === 'string') {
+                        let data = JSON.parse(notification)
+                        callback(data)
+                    } else {
+                        throw "Invalid data provided";
+                    }
+                } catch (e) {
+                    errorCallback(e)
+                }
+            })
+        } else {
+            notificationModule.getNotificationData(notification => {
+                console.log('bno ',notification)
+                // try {
+                //     if (typeof notification === 'string') {
+                //         let data = JSON.parse(notification)
+                //         callback(data)
+                //     } else {
+                //         throw "Invalid data provided";
+                //     }
+                // } catch (e) {
+                //     errorCallback(e)
+                // }
+            })
+        }
+    },
+    onMessageReceived: (callback) => {
+            eventEmitter.addListener('notificationReceived', (event) => {
+                console.log(event)
+                if (event) {
+                    let data = event
+                    callback(data)
+                }
+            })
 
-export default BlitzNotifications;
+    },
+    onNotificationTapped: (callback) => {
+        if (Platform.OS === 'android') {
+            eventEmitter.addListener('onNotificationTapped', (event) => {
+                callback(event)
+            })
+        } else {
+            console.warn('getLastNotificationData is only available on android platform')
+        }
+    }
+
+}
